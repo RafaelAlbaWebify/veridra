031a671407f7f174445da4f3d6d057d0c4c20c59
Rafael Alba <ralbas.int@gmail.com>
2026-07-22 12:33:42 +0200

    Register crawl profile delivery routes

--- src/veridra/runtime.py
+++ src/veridra/runtime.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from .app import app as app
+from .crawl_profile_web import router as crawl_profile_router
 from .lead_web import router as lead_router
 from .pdf_web import router as pdf_router
 from .task_web import router as task_router
@@ -8,6 +9,7 @@ from .task_web import router as task_router
 app.include_router(task_router)
 app.include_router(lead_router)
 app.include_router(pdf_router)
+app.include_router(crawl_profile_router)
 
 
 def main() -> None:
