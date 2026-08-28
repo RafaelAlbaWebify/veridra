e8f783b78d9d48e03b7a05f7ec116d64de8ba315
Rafael Alba <ralbas.int@gmail.com>
2026-08-28 09:32:53 +0200

    Register agency customer onboarding routes

--- src/veridra/runtime.py
+++ src/veridra/runtime.py
@@ -8,6 +8,7 @@ from . import public_web
 from .access_logging import StructuredAccessLogMiddleware, configure_access_logger
 from .agency_conversion_web import router as agency_conversion_router
 from .agency_crawl_profile_web import router as agency_crawl_profile_router
+from .agency_customer_web import router as agency_customer_router
 from .agency_lead_form_web import router as agency_lead_form_router
 from .agency_lead_web import router as agency_lead_router
 from .agency_monitoring_web import router as agency_monitoring_router
@@ -156,6 +157,7 @@ app.include_router(tenant_team_router)
 app.include_router(workspace_members_router)
 app.include_router(member_assignments_router)
 app.include_router(agency_workflow_router)
+app.include_router(agency_customer_router)
 app.include_router(agency_project_index_router)
 app.include_router(agency_conversion_router)
 app.include_router(agency_crawl_profile_router)
