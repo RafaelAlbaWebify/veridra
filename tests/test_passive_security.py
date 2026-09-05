3ba6925ea0ff6dbb90fede6435995e95a554ed67
Rafael Alba <ralbas.int@gmail.com>
2026-09-05 08:35:05 +0200

    Test active insecure resource semantics

--- tests/test_passive_security.py
+++ tests/test_passive_security.py
@@ -82,3 +82,41 @@ def test_relative_same_origin_form_is_not_cross_origin() -> None:
         "security.cross-origin-forms"
     ]
     assert finding.status == Status.passed
+
+
+def test_http_anchor_and_profile_metadata_are_not_active_resources() -> None:
+    findings = _by_id(
+        """<a href='http://example.net/page'>Legacy link</a>
+        <a href='http://facebook.com/sharer.php'>Share</a>
+        <link rel='profile' href='http://gmpg.org/xfn/11'>"""
+    )
+    finding = findings["security.insecure-resources"]
+    assert finding.status == Status.passed
+    assert finding.title == "Insecure active resources"
+
+
+def test_http_active_subresources_are_reported() -> None:
+    findings = _by_id(
+        """<script src='http://cdn.example/script.js'></script>
+        <img src='http://cdn.example/image.jpg'>
+        <link rel='stylesheet' href='http://cdn.example/site.css'>
+        <video poster='http://cdn.example/poster.jpg'></video>"""
+    )
+    finding = findings["security.insecure-resources"]
+    assert finding.status == Status.attention
+    affected = finding.evidence["affected_pages"]
+    assert affected[0]["resources"] == [
+        "http://cdn.example/script.js",
+        "http://cdn.example/image.jpg",
+        "http://cdn.example/site.css",
+        "http://cdn.example/poster.jpg",
+    ]
+
+
+def test_non_resource_link_rel_does_not_count_as_active_resource() -> None:
+    findings = _by_id(
+        """<link rel='canonical' href='http://example.com/'>
+        <link rel='profile' href='http://gmpg.org/xfn/11'>
+        <link rel='alternate' href='http://example.com/feed'>"""
+    )
+    assert findings["security.insecure-resources"].status == Status.passed
